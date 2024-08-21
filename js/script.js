cf62495806ed6c18147f91d64dbc564877cbe274
Michel Ribeiro <michelrsrs@gmail.com>
2024-08-20 23:56:20 -0300

    alteracoes javascript para otimização tela orcamento e perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+//Ativar link
 const links = document.querySelectorAll(".header-menu a");
-
 
 function ativarLink(link) {
   const url = window.location.href;
@@ -9,5 +9,40 @@ function ativarLink(link) {
   }
 }
 
-
 links.forEach(ativarLink);
+
+//Ativar Items do Orçamento;
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true
+  }
+}
+
+parametros.forEach(ativarProduto)
+
+// Ativar Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(resposta)
+
+}
+
+function eventoPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventoPerguntas);
